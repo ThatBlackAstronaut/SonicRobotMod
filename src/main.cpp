feb226fea1c4d7f0cd19a130b21d9e05c55274e1
Saritahhh <tusmuertos018@gmail.com>
2025-03-13 01:26:10 -0600

    Remove Unleashed stuff in favor of Unleashed Experience mod

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,5 @@
 #include <Geode/modify/PlayerObject.hpp>
-#include <Geode/modify/PlayLayer.hpp>
-#include <Geode/modify/EndLevelLayer.hpp>
-#include <Geode/binding/GJGameLevel.hpp>
 #include <Geode/loader/SettingV3.hpp>
-#include <random>
 
 using namespace geode::prelude;
 
@@ -19,8 +15,6 @@ auto globalSounds = Mod::get()->getSettingValue<bool>("global-sfx");
 auto sonicBall = Mod::get()->getSettingValue<bool>("sonic-ball");
 auto sonicCube = Mod::get()->getSettingValue<bool>("sonic-cube");
 auto jumpInCube = Mod::get()->getSettingValue<bool>("jumpsfx-incube");
-auto unleashedSounds = Mod::get()->getSettingValue<bool>("unleashed-sfx");
-auto unleashedRankings = Mod::get()->getSettingValue<bool>("unleashed-rankings");
 auto doIdleAnim = false;
 
 // Individual SFX settings
@@ -81,20 +75,6 @@ $on_mod(Loaded) {
     listenForSettingChanges("jumpsfx-incube", [](bool value) {
         jumpInCube = value;
     });
-    listenForSettingChanges("unleashed-sfx", [](bool value) {
-        unleashedSounds = value;
-    });
-    listenForSettingChanges("unleashed-rankings", [](bool value) {
-        unleashedRankings = value;
-    });
-}
-
-int genRandomInt(int min, int max) {
-    std::random_device rd;  // Obtain a random number from hardware
-    std::mt19937 gen(rd()); // Seed the generator
-    std::uniform_int_distribution<> distr(min, max); // Define the range
-
-    return distr(gen);
 }
 
 class $modify(PlayerObject) {
@@ -155,10 +135,6 @@ class $modify(PlayerObject) {
                 fields->m_customSprite->setVisible(false);
                 fields->m_customSprite->setID("sonic-anim"_spr);
                 this->addChild(fields->m_customSprite, 10);
-            }
-
-            if (chosenGameSprite == "classic1" && fields->m_customSprite) {
-                fields->m_idleAnim = CCSprite::createWithSpriteFrameName("classic1_sonicIdle_01.png"_spr);
             }
 
             // Hide robot
@@ -430,11 +406,6 @@ class $modify(PlayerObject) {
             }
         }
 
-        if (unleashedSounds) {
-            auto fmod = FMODAudioEngine::sharedEngine();
-            fmod->playEffect("death_1.ogg"_spr);
-        }
-
     }
 
     void startDashing(DashRingObject* p0){
@@ -456,58 +427,10 @@ class $modify(PlayerObject) {
                 fmod->playEffect(sfxToPlayDashStart);
             }
         }
-
-        // Check for unleashedSounds and play a random boost sound
-        if (unleashedSounds) {
-            int randomBoost = genRandomInt(1, 7);
-            auto sfxToPlayBoost = fmt::format("boost_{}.ogg"_spr, randomBoost);
-
-            // Generate another random int to decide whether to play the sound
-            int playSound = genRandomInt(1, 10);
-
-            if (playSound > 7) {
-                if (!globalSounds) {
-                    if (m_isRobot) {
-                        fmod->playEffect(sfxToPlayBoost);
-                    }
-                } else {
-                    fmod->playEffect(sfxToPlayBoost);
-                }
-            }
-        }
     }
 
     void flipGravity(bool p0, bool p1) {
         PlayerObject::flipGravity(p0, p1);
-
-        bool m_isCube = !m_isShip && !m_isBird && !m_isBall && !m_isDart && !m_isRobot && !m_isSpider && !m_isSwing;
-
-        if (sonicCube && m_isCube) {
-            float scaleX = m_vehicleSize;
-            float scaleY = m_vehicleSize;
-
-            if (m_isUpsideDown && m_isSideways) {
-                // Right (rotated 90deg)
-                this->setScaleX(scaleX);
-                this->setScaleY(scaleY);
-            } else if (!m_isUpsideDown && m_isSideways) {
-                // Left (rotated -90deg)
-                this->setScaleX(-scaleX);
-                this->setScaleY(scaleY);
-            } else if (m_isUpsideDown && !m_isSideways) {
-                // Upside down
-                this->setScaleX(scaleX);
-                this->setScaleY(-scaleY);
-            } else {
-                // Normal
-                this->setScaleX(scaleX);
-                this->setScaleY(scaleY);
-            }
-        }
-    }
-
-    void togglePlayerScale(bool p0, bool p1) {
-        PlayerObject::togglePlayerScale(p0, p1);
 
         bool m_isCube = !m_isShip && !m_isBird && !m_isBall && !m_isDart && !m_isRobot && !m_isSpider && !m_isSwing;
 
@@ -585,31 +508,19 @@ class $modify(PlayerObject) {
                 if (sonicCube && m_isCube && jumpInCube) {
                     fmod->playEffect(sfxToPlayJump);
                 }
-
-                // Check for unleashedSounds and play a random jump sound
-                if (unleashedSounds) {
-                    int randomJump = genRandomInt(1, 5);
-                    auto sfxToPlayRandomJump = fmt::format("jump_{}.ogg"_spr, randomJump);
-
-                    // Generate another random int to decide whether to play the sound
-                    int playSound = genRandomInt(1, 10);
-
-                    if (playSound > 5) {
-                        if (m_isRobot || (sonicCube && m_isCube && jumpInCube)) {
-                            fmod->playEffect(sfxToPlayRandomJump);
-                        }
-                    }
-                }
             } else {
                 if (m_ringJumpRelated){
-                    if (m_isRobot){
-                        fmod->playEffect(sfxToPlayOrb);
+                    if (!globalSounds) {
+                        if (m_isRobot){
+                            fmod->playEffect(sfxToPlayOrb);
+                        }
                     } else {
                         fmod->playEffect(sfxToPlayOrb);
                     }
                 }
             }
         }
+
     }
 
     void setVisible(bool visible) {
@@ -624,143 +535,4 @@ class $modify(PlayerObject) {
         }
 
     }
-
-    void updateTimeMod(float p0, bool p1) {
-        
-        PlayerObject::updateTimeMod(p0, p1);
-
-        if (unleashedSounds && PlayLayer::get()) {
-            auto fmod = FMODAudioEngine::sharedEngine();
-            int randomBoost = genRandomInt(1, 7);
-            auto sfxToPlayBoost = fmt::format("boost_{}.ogg"_spr, randomBoost);
-
-            int doPlaySound = genRandomInt(1, 10);
-
-            if (p0 >= 1.5f) {
-                fmod->playEffect("modern-fullboost.ogg"_spr);
-                if (doPlaySound >= 3) {
-                    fmod->playEffect(sfxToPlayBoost);
-                }
-            }
-        }
-    }
-};
-
-class $modify(PlayLayer) {
-    bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
-        if (!PlayLayer::init(level, useReplay, dontCreateObjects)) return false;
-
-        if (unleashedSounds) {
-            auto fmod = FMODAudioEngine::sharedEngine();
-            fmod->playEffect("herewe.ogg"_spr);
-        }
-        
-        return true;
-    }
-
-    void startGame() {
-        PlayLayer::startGame();
-
-        if (unleashedSounds) {
-            auto fmod = FMODAudioEngine::sharedEngine();
-            fmod->playEffect("go.ogg"_spr);
-        }
-    }
-
-    void levelComplete() {
-        PlayLayer::levelComplete();
-        auto fmod = FMODAudioEngine::sharedEngine();
-
-        if (unleashedSounds){
-            fmod->playEffect("goal_ring.ogg"_spr);
-        }
-
-        if(unleashedRankings){
-
-            int attempts = this->m_attempts;
-
-            int randomBest = genRandomInt(1, 6);
-            int randomGood = genRandomInt(1, 5);
-            int randomBad = genRandomInt(1, 5);
-
-            if (attempts <= 3) {
-                fmod->playEffect(fmt::format("complete_best_{}.ogg"_spr, randomBest));
-            } else if (attempts > 3 && attempts <= 10) {
-                fmod->playEffect(fmt::format("complete_good_{}.ogg"_spr, randomGood));
-            } else {
-                fmod->playEffect(fmt::format("complete_bad_{}.ogg"_spr, randomBad));
-            }
-
-        }
-    }
-};
-
-class $modify(EndLevelLayer) {
-    void customSetup() {
-        EndLevelLayer::customSetup();
-
-        if (unleashedRankings) {
-            int attempts = m_playLayer->m_attempts;
-            auto fmod = FMODAudioEngine::sharedEngine();
-
-            auto rankSprite = CCSprite::createWithSpriteFrameName("rank_placeholder.png"_spr);
-            rankSprite->setOpacity(0);
-            rankSprite->setZOrder(10);
-            rankSprite->setPosition({50, 50});
-            rankSprite->setID("rank-sprite"_spr);
-            rankSprite->setScale(9.0f);
-            this->addChild(rankSprite);
-
-            geode::log::debug("attempts: {}", attempts);
-
-            bool rankS = attempts == 1;
-            bool rankA = attempts == 2 || attempts == 3;
-            bool rankB = attempts >= 4 && attempts <= 6;
-            bool rankC = attempts >= 7 && attempts <= 10;
-            bool rankD = attempts >= 11 && attempts <= 15;
-            bool rankE = attempts > 16;
-
-            if (rankS) {
-                rankSprite->setDisplayFrame(CCSpriteFrameCache::sharedSpriteFrameCache()->spriteFrameByName("rank_s.png"_spr));
-                fmod->playEffect("rankS.ogg"_spr);
-            } else if (rankA) {
-                rankSprite->setDisplayFrame(CCSpriteFrameCache::sharedSpriteFrameCache()->spriteFrameByName("rank_a.png"_spr));
-                fmod->playEffect("rankA.ogg"_spr);
-            } else if (rankB) {
-                rankSprite->setDisplayFrame(CCSpriteFrameCache::sharedSpriteFrameCache()->spriteFrameByName("rank_b.png"_spr));
-                fmod->playEffect("rankC.ogg"_spr);
-            } else if (rankC) {
-                rankSprite->setDisplayFrame(CCSpriteFrameCache::sharedSpriteFrameCache()->spriteFrameByName("rank_c.png"_spr));
-                fmod->playEffect("rankC.ogg"_spr);
-            } else if (rankD) {
-                rankSprite->setDisplayFrame(CCSpriteFrameCache::sharedSpriteFrameCache()->spriteFrameByName("rank_d.png"_spr));
-                fmod->playEffect("rankC.ogg"_spr);
-            } else if (rankE) {
-                rankSprite->setDisplayFrame(CCSpriteFrameCache::sharedSpriteFrameCache()->spriteFrameByName("rank_e.png"_spr));
-                fmod->playEffect("rankE.ogg"_spr);
-            }
-        }
-        
-    }
-
-    void onHideLayer(CCObject* sender) {
-        EndLevelLayer::onHideLayer(sender);
-        geode::log::debug("when escondes el endlevel layer");
-    }
-
-    void showLayer(bool p0) {
-        EndLevelLayer::showLayer(p0);
-
-        if (unleashedRankings) {
-            auto ranking = this->getChildByID("rank-sprite"_spr);
-
-            // animations
-            auto fadeIn = CCFadeIn::create(0.3f);
-            auto scaleDown = CCScaleTo::create(0.3, 1.75f);
-
-            ranking->runAction(fadeIn);
-            ranking->runAction(scaleDown);
-        }
-    }
-
 };
